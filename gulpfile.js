1bb0351ceae197068d3b86cc2728b3c1cd05512e
[John Patterson] <pattersonjc1@gmail.com>
2016-10-24 07:23:54 -0400

    added links in footer

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,16 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 const babel = require('gulp-babel');
-const eslint = require('gulp-eslint');
-const notify = require("gulp-notify");
+// const eslint = require('gulp-eslint');
+// const notify = require("gulp-notify");
 
-gulp.task('lint', () => {
-    return gulp.src(['js/*.js', '!node_modules/**'])
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError())
-        .on("error", notify.onError('Error!'));
-});
+// gulp.task('lint', () => {
+//     return gulp.src(['js/*.js', '!node_modules/**'])
+//         .pipe(eslint())
+//         .pipe(eslint.format())
+//         .pipe(eslint.failAfterError())
+//         .on("error", notify.onError('Error!'));
+// });
 
 gulp.task('babel', () => {
     return gulp.src('js/*.js')
