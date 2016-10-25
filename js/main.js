46dafffb6f2a24673de181772b63387d6773721d
[John Patterson] <pattersonjc1@gmail.com>
2016-10-24 20:00:00 -0400

    more styling changes

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ $.ajax({
             <a href="https://medium.com/@johncpatterson">${result.blog}</a>...
             `)
         $('#joined').append(`
-            ${result.created_at}
+            ${moment(result.created_at).format("MMMM Do YYYY")}
             `)
         $('#followers').prepend(`
             ${result.followers} <br>
@@ -46,15 +46,18 @@ $.ajax({
     success: function(data) {
         console.log(data);
         var arrayOfRepos = data.forEach(function(repos) {
-            $('#repository-data-goes-here').append(`
+           $('#repository-data-goes-here').append(`
             <div class="repo-listing-container">
                 <div class="repos-name"><a href="#">${repos.name}</a></div>
                 <div class="repos-forks-count"><img src="img/git-branch.svg" class="git-branch">${repos.forks}</div>
                 <div class="repos-stars-count"><img src="img/star.svg" class="star">${repos.stargazers_count}</div>
                 <div class="repos-language">${repos.language}</div>
-                <div class="updated-at">Updated ${repos.updated_at}</div>
+                <div class="updated-at">Updated ${moment(repos.updated_at).fromNow()}</div>
             </div>
             `)
+
+
+
         })
     }
 })
@@ -64,16 +67,21 @@ $.ajax({
     success: function(result) {
         console.log(result);
         var arrayOfEvents = result.forEach(function(events) {
-            // var commitKey = events.payload.head;
-            // console.log(commitKey);
-            // var commitKeyLast5 = commitKey.slice(-7);     <------ this was my attempt to limit the character count of the commit string
+
+
+
+            if(events.type === "PushEvent") {
+            var commitKey = events.payload.head;
+            console.log(commitKey);
+            var commitKeyLast5 = commitKey.substr(0,7);   
             $('#event-data-goes-here').append(`
             <div class="events-listing-container">
-                <div class=""><img src="img/git-commit.svg" class="git-commit-logo">${events.created_at}</div>
+                <div class=""><img src="img/git-commit.svg" class="git-commit-logo">${moment(events.created_at).fromNow()}</div>
                 <div class="pushed-to"><a href="https://github.com/johncpatterson">${events.actor.login}</a> pushed to ${events.payload.ref} at ${events.repo.name}</div>
-                <div class="profile-pic-and-commit"><img src="${events.actor.avatar_url}" class="events-profile-pic-large"><img src="${events.actor.avatar_url}" class="events-profile-pic-small">${events.payload.head}</div>
+                <div class="profile-pic-and-commit"><img src="${events.actor.avatar_url}" class="events-profile-pic-large"><img src="${events.actor.avatar_url}" class="events-profile-pic-small">${commitKeyLast5}</div>
             </div>
             `)
+            }
         })
     }
 })
